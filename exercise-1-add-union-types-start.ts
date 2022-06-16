a41ab5533616c31c5ff06d71631e4510e1aac8a9
Simone D'Ambrosio <simonedamb@hotmail.it>
2022-06-16 17:59:26 +0200

    fixed-

--- exercise-1-add-union-types-start.ts
+++ exercise-1-add-union-types-start.ts
@@ -8,18 +8,18 @@
 //
 // This should fix the type error on Line 15.
 
-type Currency = string;
+type Currency = string | boolean;
 
 let indianCurrency: Currency = "Indian rupee";
 
-let narniaCurrency: Currency = "false";
+let narniaCurrency: Currency = false;
 
 // Change the type for the `data` function parameter so it can accept
 // the different types of data that are passed when it's being called.
 //
 // This should fix the type error on Lines 28 and 30.
 
-function outputCountryData(country: any, data: any) {
+function outputCountryData(country: string, data: number | string) {
   console.log(`The data for ${country} is: ${data}`);
 }
 
